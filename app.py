0cd0b5c9bb058c111bd5cd42b729fdf2ef1141be
Ionics <142382277+Ionecs-1@users.noreply.github.com>
2025-04-26 16:22:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -46,12 +46,15 @@ bcrypt = Bcrypt(app)
 class User(db.Model):   
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True) 
-    name = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False, index=True)
     phone_number = db.Column(db.String(15), nullable=False)
     password = db.Column(db.String(128), nullable=False)
     user_type = db.Column(db.String(20), nullable=False)
 
+    transactions_sent = db.relationship('Transaction', foreign_keys='Transaction.sender_id', back_populates='user', cascade="all, delete-orphan")
+    transactions_received = db.relationship()
+    
 class Skill(db.Model):
     __tablename__ = 'skill'
     id = db.Column(db.Integer, primary_key=True)
@@ -60,6 +63,8 @@ class Skill(db.Model):
     proficiency = db.Column(db.Integer, nullable=False)
     years_of_experience = db.Column(db.Integer, nullable=False)
     user = db.relationship('User', backref='skills')
+
+    user = db.relationship()
     
 class Task(db.Model):
     __tablename__ = 'task'
@@ -70,7 +75,7 @@ class Task(db.Model):
     location = db.Column(db.String(100), nullable=False)
     budget = db.Column(db.Float, nullable=False)
     deadline = db.Column(db.String(20), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) 
     created = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.func.now())
     worker_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
     status = db.Column(db.String(20), default='Open')
@@ -82,10 +87,15 @@ class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     receiver_id = db.Column(db.Integer, db.ForeginKey('user.id'), nullable=False)
-    task_id = db.Column(db.Float, nullable=False)
+    task_id = db.Column(db.Integer, db.ForeginKey('task.id') nullable=False)
     amount = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(255))
     timestamp = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
     status = db.Column(db.String(20), nullable=False, default='Pending')
+
+    sender = db.relationships()
+    receiver = db.relationships()
+    task = db.relationships()
     
 # Create the database and tables
 with app.app_context():
