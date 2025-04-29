cc5fbff20d740e0d9bdb43f8a1d6e718be9e0333
Ionics <142382277+Ionecs-1@users.noreply.github.com>
2025-04-28 21:57:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    task_id = db.Column(db.Integer, db.ForeignKey('task.id') nullable=False)
+    task_id = db.Column(db.Integer, db.ForeignKey('task.id'), nullable=False)
     amount = db.Column(db.Float, nullable=False)
     description = db.Column(db.String(255))
     timestamp = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
